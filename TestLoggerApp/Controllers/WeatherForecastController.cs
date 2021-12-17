2862ea289a53b482b73e923058fbab782bf21007
Akash Lissy <Akashmdr90@hotmail.com>
2021-12-16 23:41:46 -0600

    Intial Commit

---
+++ TestLoggerApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestLoggerApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        public IActionResult Error()
+        {
+            _logger.LogInformation("Getting random items. There are {AvailableItems} possible values", Summaries.Count());
+
+            _logger.LogWarning("This is a warning");
+
+            try
+            {
+                throw new ArgumentException();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "And this is an error");
+            }
+            
+            return Ok();            
+        }
+
+    }
+}
